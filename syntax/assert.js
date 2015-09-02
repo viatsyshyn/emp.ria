b584e2ded14c1393e70d35617bdabe7c88cb83b1
Volodymyr Iatsyshyn <viatsyshyn@hellowebapps.com>
2015-09-02 18:15:44 +0300

    improved asserts' debug feature

--- syntax/assert.js
+++ syntax/assert.js
@@ -7,6 +7,8 @@ ria.__CFG.AssertWithExceptions = !!ria.__CFG.AssertWithExceptions;
 (function () {
     "use strict";
 
+    var skipMe = {};
+
     /**
      *
      * @param {Boolean} condition
@@ -20,8 +22,18 @@ ria.__CFG.AssertWithExceptions = !!ria.__CFG.AssertWithExceptions;
             throw Error('Assert failed with msg "' + msg_ + '"');
         }
 
-        if (confirm('Assert failed with msg "' + msg_ + '". Debug?'))
-            debugger;
+        var msg = 'Assert failed with msg "' + msg_ + '". Debug?';
+        if (skipMe[msg] || !confirm(msg)) {
+          skipMe[msg] = new Date();
+
+          if (console.trace) {
+            console.trace(msg);
+          } else {
+            console.error('Assert failed with msg "' + msg_ + '". Debug?');
+          }
+        } else {
+          debugger;
+        }
     }
 
     ria.__SYNTAX.Assert = Assert;
